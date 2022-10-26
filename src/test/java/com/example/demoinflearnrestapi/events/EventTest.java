e68008f360888b95862bb229e998e1ba1e0245f3
BeobSik <tjswl756@naver.com>
2022-10-26 14:14:29 +0900

    Add Event domain

---
+++ src/test/java/com/example/demoinflearnrestapi/events/EventTest.java
@@ -0,0 +1,31 @@
+package com.example.demoinflearnrestapi.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder().build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean() {
+        // Given
+        Event event = new Event();
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
