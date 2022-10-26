c230414fdc0cdbb211aa5d256e3ea90cade53ad0
BeobSik <tjswl756@naver.com>
2022-10-26 15:06:09 +0900

    Modify createEventTest

--- src/test/java/com/example/demoinflearnrestapi/events/EventControllerTests.java
+++ src/test/java/com/example/demoinflearnrestapi/events/EventControllerTests.java
@@ -1,5 +1,6 @@
 package com.example.demoinflearnrestapi.events;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,11 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
@@ -20,11 +24,31 @@ public class EventControllerTests {
     @Autowired
     MockMvc mockMvc;
 
+    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void createEvent() throws Exception {
+        Event event = Event.builder()
+                .name("Spring")
+                .description("REST API Development with Spring")
+                .beginEnrollmentDateTime(LocalDateTime.of(2022, 10, 25, 0, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2022, 10, 25, 23, 59))
+                .beginEventDateTime(LocalDateTime.of(2022, 10, 26, 14, 51))
+                .endEventDateTime(LocalDateTime.of(2022, 10, 26, 15, 0))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(200)
+                .location("상도동 리치빈스")
+                .build();
+
         mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON)
-                    .accept(MediaTypes.HAL_JSON))
-                .andExpect(status().isCreated());
+                    .accept(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(event)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+                .andDo(print());
     }
 }
