191ad9b79151be4e4c9f65315d09e3b2eab33017
BeobSik <tjswl756@naver.com>
2022-10-27 15:55:59 +0900

    Add AccountRepository, AccountService,  spring-security-oauth2-autoconfigure

---
+++ src/test/java/com/example/demoinflearnrestapi/accounts/AccountServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.demoinflearnrestapi.accounts;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class AccountServiceTest {
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountRepository accountRepository;
+    @DisplayName("이름으로 사용자 찾기")
+    @Test
+    public void findByUsername() {
+        // Given
+        String email = "test@email.com";
+        @SuppressWarnings("SpellCheckingInspection")
+        String password = "beobsik";
+        Account account = Account.builder()
+                .email(email)
+                .password(password)
+                .roles(Set.of(AccountRole.ADMIN, AccountRole.USER))
+                .build();
+        this.accountRepository.save(account);
+
+        // When
+        UserDetailsService userDetailsService = accountService;
+        UserDetails userDetails = userDetailsService.loadUserByUsername(email);
+
+        // Then
+        assertThat(userDetails.getPassword()).isEqualTo(password);
+    }
+}
